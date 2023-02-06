2d09ad209cabed485e35465fc1f84ba4321b843b
ragan <raganalemu3@gmail.com>
2023-02-06 13:39:30 +0300

    added model

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "./components/inputField";
 import "./App.css";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div className="App">
       <span className="heading">Tasks</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/inputField.tsx
+++ src/components/inputField.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="text" placeholder="Enter a Task" className="input__box" />
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a Task"
+        className="input__box"
+      />
       <button type="submit" className="input_submit">
         GO
       </button>
